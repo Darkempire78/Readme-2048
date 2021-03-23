1c3841643e8219c7a7831ba4fe81945817bd06c1
Darkempire78 <clement.wawszczyk@gmail.com>
2021-03-23 19:38:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-import os
-
 from github import Github
 
-print('test')
+import os
+
+
+def main():
+    g = Github(os.environ["GITHUB_TOKEN"])
+    repo = g.get_repo(os.environ["GITHUB_REPOSITORY"])
+    issue = repo.get_issue(number=int(os.environ["ISSUE_NUMBER"]))
+
+    issueTitle  = issue.title
+    issueAuthor = "@" + issue.user.login
+
+    print("test")
+    issue.create_comment(f"{issueAuthor} --- {issueTitle} test!")
+
+
+
+if __name__ == "__main__":
+	main() 
