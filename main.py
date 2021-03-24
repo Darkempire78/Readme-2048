43bd20c250b179a568f96bd3a82b5c20aa3b3718
Darkempire78 <clement.wawszczyk@gmail.com>
2021-03-24 22:11:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -96,6 +96,8 @@ def endAction(grid, score, issue, issueAuthor, issueText):
     # Update current.json
     with open("Data/Games/current.json", "r") as _current:
         current = json.load(_current)
+        print("current[score] : ", current["score"])
+        print("score : ", score)
         current["score"] += score
         current["grid"] = grid
     with open("Data/Games/current.json", "w") as _current:
